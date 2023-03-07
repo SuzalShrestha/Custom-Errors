126d2f8aaecdf8a33de41ab2bbc13786317b56f3
sujal_shrestha <sujal.scxi121@amv.edu.np>
2023-03-07 08:39:51 +0545

    captureStackTrace bug

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,7 +1,19 @@
 const ValidationError = require('./validation-error');
 
 // Your code here
-
+class MaximumLengthExceededError extends ValidationError{
+  constructor(excessLength,message="Maximum length exceeded"){
+    super(message);
+    if(excessLength!==undefined){
+    message=`Maximum length exceeded by ${excessLength}`;
+    }
+    this.message=message;
+    if(Error.captureStackTrace){
+      Error.captureStackTrace(this, MaximumLengthExceededError);
+    }    
+    this.name = "MaximumLengthExceededError";
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
